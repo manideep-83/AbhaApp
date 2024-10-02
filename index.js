e962dff54749c5c6f6599a67f39279e6498dfdf1
manideep-83 <mbustudy@gmail.com>
2024-10-02 14:56:37 +0530

    Updated index.js with new changes

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app=express();
 var cors=require('cors');
 app.use(cors());
 app.use(express.json());
-
+app.get('/',(req,res)=>{
+    res.send("This is starting of Abha app Testing backend");
+});
 app.listen(5000,()=>{
     console.log("connection established");
 });
